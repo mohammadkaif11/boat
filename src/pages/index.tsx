8d3135c870cca3fefd28e979b0cbd7c27fde4cf5
Mohammad kaif <83584426+mohammadkaif11@users.noreply.github.com>
2023-08-23 16:00:51 +0530

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import HomePage from "../component/HomePage/HomePage";
+import Data from "../../template.json";
+import NameKey from "../../data.json";
+
+export default function Home({data}:any) {
+  return (
+    <main>
+      <HomePage WebContent={data}/>
+    </main>
+  );
+}
+
+
+export async function getStaticProps() {
+  if(NameKey.Name==""){
+    return {
+        props: {
+          data: Data,
+        },
+      };
+  }else{
+    try {
+      const WebName=NameKey.Name;
+      const response=await fetch(`https://ftest-api.onrender.com/ftests/${WebName}`);
+      const responseData=await response.json();
+      return {
+          props: {
+            data:responseData,
+          },
+        };
+    } catch (error) {
+      console.log('Error ',error)
+      return {
+        props: {
+          data:Data,
+        },
+      };
+    }
+  }
+}
